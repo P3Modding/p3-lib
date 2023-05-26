b2d536bdfdf63d61b451c6b1152d93105b01e2fd
Benedikt Radtke <benediktradtke@gmail.com>
2023-05-26 20:54:40 +0200

    fix buildscript

--- p3-aim-sys/build.rs
+++ p3-aim-sys/build.rs
@@ -1,5 +1,10 @@
 fn main() {
-    println!("cargo:rustc-link-search=C:\\Users\\Benni\\repositories\\p3-lib\\p3-aim-sys\\blobs");
     println!("cargo:rustc-link-lib=AIM");
     println!("cargo:rustc-link-lib=ijl11");
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    println!("cargo:rustc-link-search=native={}", out_dir.to_str().unwrap());
+    std::fs::copy("blobs/AIM.dll", out_dir.join("AIM.dll")).unwrap();
+    std::fs::copy("blobs/AIM.lib", out_dir.join("AIM.lib")).unwrap();
+    std::fs::copy("blobs/ijl11.dll", out_dir.join("ijl11.dll")).unwrap();
+    std::fs::copy("blobs/ijl11.lib", out_dir.join("ijl11.lib")).unwrap();
 }
