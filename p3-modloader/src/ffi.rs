79fceb17463d0249d4282cf013f5f1e3d874abb1
Benedikt Radtke <benediktradtke@gmail.com>
2024-09-15 17:47:27 +0200

    don't speficy p3.exe's name

--- p3-modloader/src/ffi.rs
+++ p3-modloader/src/ffi.rs
@@ -3,8 +3,7 @@ use std::sync::atomic::{AtomicPtr, Ordering};
 
 use hooklet::{hook_call_rel32, CallRel32Hook};
 use log::{debug, error};
-use windows::core::PSTR;
-use windows::s;
+use windows::core::{PCSTR, PSTR};
 use windows::Win32::Foundation::HMODULE;
 use windows::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
 
@@ -18,7 +17,7 @@ extern "system" fn DllMain(_hist_dll: *const u8, fdw_reason: u32, _lpv_reserved:
         log::set_max_level(log::LevelFilter::Trace);
         debug!("DllMain patching WinMain call");
         unsafe {
-            let hook = match hook_call_rel32(s!("Patrician3_patched.exe"), 0x0023CA22, WinMain_hook as usize) {
+            let hook = match hook_call_rel32(PCSTR::null(), 0x0023CA22, WinMain_hook as usize) {
                 Ok(hook) => hook,
                 Err(e) => {
                     error!("Failed to set hook: {:?}", e);
