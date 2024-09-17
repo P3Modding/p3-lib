da472dbe2d70445be241d147cb68e0000d34e3e5
Benedikt Radtke <benediktradtke@gmail.com>
2024-09-17 08:10:44 +0200

    add increase alderman found settlement mission limit mod

---
+++ mod-increase-alderman-found-settlement-mission-limit/src/ffi.rs
@@ -0,0 +1,32 @@
+use log::{debug, error};
+use windows::Win32::{
+    Foundation::{GetLastError, WIN32_ERROR},
+    System::Memory::{VirtualProtect, PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS},
+};
+
+const PATCH_ADDRESS: u32 = 0x0053275F;
+
+#[no_mangle]
+pub unsafe extern "C" fn start() -> u32 {
+    let _ = log::set_logger(&win_dbg_logger::DEBUGGER_LOGGER);
+    log::set_max_level(log::LevelFilter::Trace);
+    let patch_ptr: *mut u8 = PATCH_ADDRESS as _;
+
+    let mut old_flags: PAGE_PROTECTION_FLAGS = windows::Win32::System::Memory::PAGE_PROTECTION_FLAGS(0);
+    if !VirtualProtect(patch_ptr as _, 5, PAGE_EXECUTE_READWRITE, &mut old_flags).as_bool() {
+        let error: WIN32_ERROR = GetLastError();
+        error!("VirtualProtect PAGE_EXECUTE_READWRITE failed: {:?}", error);
+        return 1;
+    }
+
+    debug!("Patching comparison at {:#x}", PATCH_ADDRESS);
+    *patch_ptr = 0x28;
+
+    if !VirtualProtect(patch_ptr as _, 5, old_flags, &mut old_flags).as_bool() {
+        let error: WIN32_ERROR = GetLastError();
+        error!("VirtualProtect restore failed: {:?}", error);
+        return 2;
+    }
+
+    0
+}
