8b35a1a6685dbbad1306fd20f4e2a6d08e4d5960
Benedikt Radtke <benediktradtke@gmail.com>
2024-09-20 21:15:46 +0200

    high res works more or less

--- p3-api/src/data/mod.rs
+++ p3-api/src/data/mod.rs
@@ -38,3 +38,28 @@ pub fn ddraw_fill_solid_rect(x1: u32, y1: u32, x2: u32, y2: u32) {
     let function: extern "cdecl" fn(x1: u32, y1: u32, x2: u32, y2: u32) = unsafe { mem::transmute(0x004BB430) };
     function(x1, y1, x2, y2)
 }
+
+pub fn ddraw_set_render_dest(dest: i32) -> i32 {
+    let function: extern "cdecl" fn(dest: i32) -> i32 = unsafe { mem::transmute(0x004BB9B0) };
+    function(dest)
+}
+
+pub fn ddraw_set_2d_clipping_mode(mode: u32) -> u32 {
+    let function: extern "cdecl" fn(dest: u32) -> u32 = unsafe { mem::transmute(0x004BB760) };
+    function(mode)
+}
+
+pub fn ddraw_set_clip_rect(x: u32, y: u32, width: u32, height: u32) {
+    let function: extern "cdecl" fn(x: u32, y: u32, width: u32, height: u32) = unsafe { mem::transmute(0x004BB800) };
+    function(x, y, width, height)
+}
+
+pub fn ddraw_add_clip_rect(rect: [u32; 4]) {
+    let function: extern "cdecl" fn(rect: *const u32) = unsafe { mem::transmute(0x004BB140) };
+    function(rect.as_ptr())
+}
+
+pub fn ddraw_set_clip_rect_r(rect: [u32; 4]) {
+    let function: extern "cdecl" fn(rect: *const u32) = unsafe { mem::transmute(0x004BB820) };
+    function(rect.as_ptr())
+}
